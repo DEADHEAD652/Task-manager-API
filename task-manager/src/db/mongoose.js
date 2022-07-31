cb5e5409cbe14ba50b157184c2223b4c6503cbde
hamza <hamzajjmalik@gmail.com>
2022-08-01 02:07:47 +0500

    dk commit

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,56 +1,11 @@
 const mongoose = require('mongoose')
-const validator = require('validator')
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useNewUrlParser: true,
 
 })
 
 
-/* const User = mongoose.model('User', {
-    name: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    email: {
-        type: String,
-        required: true,
-        validate(value) {
-            if (!validator.isEmail(value)) {
-                throw new Error('Email is invalid')
-            }
-        },
-        trim: true,
-        lowercase: true
-
-    },
-    password: {
-        type: String,
-        required: true,
-        trim: true,
-        min: 7,
-        validate(value){
-            if(value == 'password'){
-                throw new Error( value+' can not be password')
-            }
-        }
-
-
-
-
-    },
-
-    age: {
-        type: Number,
-        default: 0,
-
-        validate(value) {
-            if (value < 0) {
-                throw new Error('Age must be positive number')
-            }
-        }
-    }
-})
+/* const 
 const user = new User({
     name: 'hamza bashir malik              ',
      password:'passwd  ',
@@ -61,29 +16,23 @@ user.save().then(() => {
     console.log(user)
 }).catch((error) => {
     console.log(error)
-}) */
+}) 
 
- const Task = mongoose.model('Task' ,{
-description : {
-    type:String,
-    required:true,
-    trim:true
-},
-IsDone:{
-    type:Boolean,
-    default:false
-}
+const Task = mongoose.model('Task', {
+    description: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    IsDone: {
+        type: Boolean,
+        default: false
+    }
 
-    
+
 })
 const task = new Task({
-description : "studying",
-IsDone:false
-    
-})
+    description: "studying",
+    IsDone: false
 
-task.save().then(() => {
-    console.log(task)
-}).catch((error) => {
-    console.log(error)
-}) 
+}) */
