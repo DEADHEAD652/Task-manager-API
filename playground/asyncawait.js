8f3eee477c19a110faf8c8d4abbf0ff0b73f2d83
hamza <hamzajjmalik@gmail.com>
2022-08-05 03:44:07 +0500

    async function

--- playground/asyncawait.js
+++ playground/asyncawait.js
@@ -1,4 +1,30 @@
-const doWork = async () =>{
+const add = (a, b) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+ if(a < 0 || b < 0){
+    return reject('number must be positive')
+ }
+
+            resolve(a + b)
+        }, 2000)
+
+    })
+
 
 }
-console.log(doWork())
+
+
+const doWork = async () => {
+    const sum = await add(1, 3)
+    const sum2 = await add(sum, 96)
+    const sum3 = await add(sum2, -1)
+    return sum3
+}
+doWork().then((result) => {
+
+    console.log('result', result)
+
+}).catch((e) => {
+
+    console.log('error', e)
+})
