0f6387c38e2b22535a85258d6dfe72b755cb1fa1
hamza <hamzajjmalik@gmail.com>
2022-08-14 08:07:06 +0500

    express middleware

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -5,7 +5,25 @@ const userRouter = require('./routers/user')
 const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.port || 3000
+//const allowedMethods = ['HEAD', 'POST']
+/*app.use((req, res, next) => {
 
+if (req.method === 'GET') {
+    res.send('Method Not Allowed')
+} else {
+    next()
+}
+}) */
+app.use((req, res, use) => {
+    if (req.method) {
+
+
+        res.send('site under construction')
+        res.status(503)
+        return use()
+    }
+
+})
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
@@ -14,7 +32,7 @@ app.use(taskRouter)
 app.listen(port, () => {
     console.log('server is on port ' + port)
 })
-  
+
 
 
 
