a19d03044cb43ddf3b89bc4fe6079137b49242bb
hamza <hamzajjmalik@gmail.com>
2022-09-02 01:23:30 +0500

    time stamping

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -15,14 +15,3 @@ app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
 
-const Task = require('./models/task')
-const User  = require('./models/user')
-const main = async () => {
-    // const task = await Task.findById('6309fe95ec0ff10661bfee52')
-    // await task.populate('owner')
-    // console.log(task.owner)
-    const user = await User.findById('6309fda1460ee35fe8d9baaa')
-    await user.populate('tasks')
-    console.log(user.tasks)
-}
-main()
--- task-manager/src/models/task.js
+++ task-manager/src/models/task.js
@@ -1,7 +1,31 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
 
-const Task = mongoose.model('Task', {
+const TaskSchema = mongoose.Schema({
+    description: {
+        trim: true,
+        required: true,
+        type: String
+
+
+    },
+    IsDone: {
+
+        type: Boolean,
+        defualt: false
+
+
+    },
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User'
+    }
+}, {
+    timestamps: true
+})
+
+/*const Task = mongoose.model('Task', {
     description: {
         trim : true,
         required: true,
@@ -26,8 +50,8 @@ const Task = mongoose.model('Task', {
 
 
 
-})
+})*/
 
 
-
+const Task = mongoose.model('Task', TaskSchema)
 module.exports = Task
